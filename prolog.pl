db1bdb412bc5be5868b4940f580bf8359d1d3100
WilliamDBell <williamdbell97@gmail.com>
2019-02-22 20:28:02 -0500

    min-above-min

--- prolog.pl
+++ prolog.pl
@@ -51,3 +51,39 @@ find_min([Head | Tail], Min1, Min0):-
   number(Head),
   Min2 is min(Head, Min1), 
   find_min(Tail, Min2, Min0).
+
+find_min_above_val([Head | Tail], Val, Min) :-
+  number(Head),
+  Head =< Val,
+  find_min_above_val(Tail, Val, Min).
+
+find_min_above_val([Head | Tail], Val, Min) :-
+  number(Head),
+  Head > Val,
+  find_min_above_val(Tail, Val, Head, Min).
+
+find_min_above_val([Head | Tail], Val, Min) :-
+  find_min_above_val(Tail, Val, Min).
+
+find_min_above_val([], Val, Min0, Min):-
+    Min is Min0. 
+
+find_min_above_val([Head | Tail], Val, Min0, Min) :-
+  number(Head),
+  Head > Val,
+  Min1 is min(Head, Min0), 
+  find_min_above_val(Tail, Val, Min1, Min).
+  
+find_min_above_val([Head | Tail], Val, Min0, Min) :-
+  number(Head),
+  Head < Val,
+  find_min_above_val(Tail, Val, Min0, Min).
+    
+ find_min_above_val([Head | Tail], Val, Min0, Min) :-
+  find_min_above_val(Tail, Val, Min0, Min).
+
+
+min-above-min(L1, L2, N) :-
+    find_min(L2, M0),
+    find_min_above_val(L1, M0, M1), 
+    M1 == N.
